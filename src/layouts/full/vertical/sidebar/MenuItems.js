16d06f6781403dc7d78856f473d06aa416de3565
leith62 <manaaleith62@gmail.com>
2024-05-05 00:32:17 +0200

    first commit

---
+++ src/layouts/full/vertical/sidebar/MenuItems.js
@@ -0,0 +1,111 @@
+import {
+  IconAward,
+  IconBoxMultiple,
+  IconPoint,
+  IconAlertCircle,
+  IconNotes,
+  IconCalendar,
+  IconMail,
+  IconTicket,
+  IconEdit,
+  IconGitMerge,
+  IconCurrencyDollar,
+  IconApps,
+  IconFileDescription,
+  IconFileDots,
+  IconFiles,
+  IconBan,
+  IconStar,
+  IconMoodSmile,
+  IconBorderAll,
+  IconBorderHorizontal,
+  IconBorderInner,
+  IconBorderVertical,
+  IconBorderTop,
+  IconUserCircle,
+  IconPackage,
+  IconMessage2,
+  IconBasket,
+  IconChartLine,
+  IconChartArcs,
+  IconChartCandle,
+  IconChartArea,
+  IconChartDots,
+  IconChartDonut3,
+  IconChartRadar,
+  IconLogin,
+  IconUserPlus,
+  IconRotate,
+  IconBox,
+  IconHelp,
+  IconBoxAlignBottom,
+  IconBoxAlignLeft,
+  IconLayout,
+  IconZoomCode,
+  IconSettings,
+  IconBorderStyle2,
+  IconAppWindow,
+  IconLockAccess,
+} from '@tabler/icons';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+  {
+    navlabel: true,
+    subheader: 'Ecommerce',
+  },
+
+
+  {
+    id: uniqueId(),
+    title: 'Shop',
+    icon: IconBasket,
+    href: '/apps/ecommerce/shop',
+  },
+  {
+    id: uniqueId(),
+    title: 'Shop Profile',
+    icon: IconBox,
+    href: '/user-profile',
+  },
+  {
+    navlabel: true,
+    subheader: 'Insurance',
+  },
+  {
+    id: uniqueId(),
+    title: 'Contracts',
+    icon: IconPackage,
+    chip: '2',
+    chipColor: 'secondary',
+    href: '/apps/contacts',
+  },
+
+  {
+    id: uniqueId(),
+    title: 'Claims',
+    icon: IconChartDonut3,
+    href: '/apps/blog/',
+  },
+  {
+    navlabel: true,
+    subheader: 'Me',
+  },
+  
+  {
+    id: uniqueId(),
+    title: 'Chats',
+    icon: IconMessage2,
+    href: '/apps/chats',
+  },
+  {
+    id: uniqueId(),
+    title: 'Account Setting',
+    icon: IconUserCircle,
+    href: '/pages/account-settings',
+  },
+ 
+];
+
+export default Menuitems;
